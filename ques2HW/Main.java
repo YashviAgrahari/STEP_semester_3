358ffff88d8735d356871ae499dd281de3fcd8ea
Yashvi Agrahari <yashviagrahari0@gmail.com>
2026-09-04 23:49:15 +0530

    item restocking
    The Main class initializes an array of Item objects and restocks each item by 20 units, printing the final stock.

---
+++ ques2HW/Main.java
@@ -0,0 +1,39 @@
+package ques2HW;
+
+class Item
+{
+    String itemName;
+    int stock;
+
+    Item(String itemName, int stock)
+    {
+        this.itemName = itemName;
+        this.stock = stock;
+    }
+
+    void restock(int stock)
+    {
+        this.stock = this.stock + stock;
+    }
+}
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Item[] items = new Item[4];
+
+        items[0] = new Item("Samosa", 15);
+        items[1] = new Item("Tea Powder", 40);
+        items[2] = new Item("Bread", 8);
+        items[3] = new Item("Biscuit Packs", 25);
+
+        for(int i = 0; i < 4; i++)
+        {
+            items[i].restock(20);
+
+            System.out.println(items[i].itemName +
+                    " | Final Stock: " + items[i].stock);
+        }
+    }
+}
