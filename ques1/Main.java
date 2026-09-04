82f8825d4295534d226fe2f9297e91a3b40b1e2c
Yashvi Agrahari <yashviagrahari0@gmail.com>
2026-09-04 22:47:17 +0530

    ques1

--- ques1/Main.java
+++ ques1/Main.java
@@ -1,7 +1,34 @@
 package ques1;
 
+
+
+class PlacementRecord {
+    String studentName;
+    String company;
+    double packageLpa;
+
+    PlacementRecord(String studentName, String company, double packageLpa) {
+        this.studentName = studentName;
+        this.company = company;
+        this.packageLpa = packageLpa;
+    }
+
+    void printRecord() {
+        System.out.println(studentName + " -> " + company + " @ " + packageLpa + " LPA");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        // Your Q1 code goes here
+
+        PlacementRecord p1 = new PlacementRecord("Ravi", "TCS", 4.5);
+        PlacementRecord p2 = new PlacementRecord("Anitha", "Zoho", 6.2);
+        PlacementRecord p3 = new PlacementRecord("Karthik", "Infosys", 4.0);
+
+        PlacementRecord[] records = {p1, p2, p3};
+
+        for (int i = 0; i < records.length; i++) {
+            records[i].printRecord();
+        }
     }
 }
