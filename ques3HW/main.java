a49c85f491df87f8a28b37a4db44dc258661731d
Yashvi Agrahari <yashviagrahari0@gmail.com>
2026-09-11 22:56:51 +0530

    Implement CineScreen class for seat management

---
+++ ques3HW/main.java
@@ -0,0 +1,39 @@
+package ques3HW;
+
+public class CineScreen
+{
+    private int seatsTotal;
+    private int seatsAvailable;
+
+    public CineScreen(int seatsTotal)
+    {
+        if (seatsTotal <= 0)
+        {
+            throw new IllegalArgumentException();
+        }
+
+        this.seatsTotal = seatsTotal;
+        this.seatsAvailable = seatsTotal;
+    }
+
+    public void bookSeat()
+    {
+        if (seatsAvailable > 0)
+        {
+            seatsAvailable--;
+        }
+    }
+
+    public void cancelBooking()
+    {
+        if (seatsAvailable < seatsTotal)
+        {
+            seatsAvailable++;
+        }
+    }
+
+    public int getSeatsAvailable()
+    {
+        return seatsAvailable;
+    }
+}
